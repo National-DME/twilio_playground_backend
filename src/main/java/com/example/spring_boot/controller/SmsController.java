3acf74430d4dfb21222b5d5f3fd3623d3060dc6b
Thomas Cottis <tcottis@domain.dme>
2024-12-05 14:37:22 -0700

    Updated request controller route

--- src/main/java/com/example/spring_boot/controller/SmsController.java
+++ src/main/java/com/example/spring_boot/controller/SmsController.java
@@ -2,7 +2,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/sms")
+@RequestMapping("/sms")
 public class SmsController {
 
     @Autowired
