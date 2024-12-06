8aef7de5093e5298e1b4880caa2ca37d4b905af3
Thomas Joseph Cottis <tcottis@nationaldme.com>
2024-12-06 16:07:46 -0700

    Updated /user/create endpoint to return string
    Next is to get twilio SDK working

--- src/main/java/com/example/spring_boot/controller/UserController.java
+++ src/main/java/com/example/spring_boot/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.spring_boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.spring_boot.entities.User;
 import com.example.spring_boot.service.UserService;
@@ -14,8 +16,13 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/create")
-    public void createUser(@RequestBody User user) {
-        userService.createUser(user.getName(), user.getPhoneNumber());
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        try {
+            userService.createUser(user.getName(), user.getPhoneNumber());
+            return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating user: " + e.getMessage());
+        }
     }
 
     @GetMapping
