9be1d50b0604c3b5d80a31bf5238e1004dde8632
Thomas Cottis <tcottis@domain.dme>
2024-12-17 13:50:57 -0700

    Removed unnecessary import

--- src/main/java/com/example/spring_boot/controller/TwilioController.java
+++ src/main/java/com/example/spring_boot/controller/TwilioController.java
@@ -4,7 +4,6 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
